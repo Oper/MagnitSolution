ff66e4f3e2485fd2889fe3a81d719ce1b47dc659
vBaykov <my@vbaykov.ru>
2017-10-15 14:12:07 +0300

    6 пункт

--- src/ru/vbaykov/magnit/Main.java
+++ src/ru/vbaykov/magnit/Main.java
@@ -66,8 +66,8 @@ public class Main {
         StreamResult out = new StreamResult(outputXSLT);
         transformer.transform(in, out);
     }
-    public List parsingXML(String dataXML)
-    {
+
+    public List parsingXML(String dataXML) {
         ArrayList<Integer> list = new ArrayList<>();
         try {
             DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
@@ -97,90 +97,82 @@ public class Main {
         // write your code here
         ArrayList<Integer> list = new ArrayList<>();
         Main main = new Main();
-        main.setN(3);
-//        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
-//            System.out.println("Введите адрес базы");
-//            main.setUrl(reader.readLine());
-//            System.out.println("Login");
-//            main.setLogin(reader.readLine());
-//            System.out.println("Password");
-//            main.setPass(reader.readLine());
-//            System.out.println("N число");
-//            main.setN(Integer.parseInt(String.valueOf(Integer.parseInt(reader.readLine()))));
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("Login");
+            main.setLogin(reader.readLine());
+            System.out.println("Password");
+            main.setPass(reader.readLine());
+            System.out.println("N число");
+            main.setN(Integer.parseInt(String.valueOf(Integer.parseInt(reader.readLine()))));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         try {
             Class.forName("org.postgresql.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/TEST", "pgadmin", "test");
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/TEST", main.getLogin(), main.getPass());
              Statement statement = connection.createStatement()) {
             //Чистим таблицу если не пустая
-            //System.out.println("Opened database successfully");
-            //if (statement.getResultSet().next())
             statement.executeUpdate("DELETE from TEST");
-            //statement.executeUpdate("CREATE TABLE TEST (FIELD INT)");
             //Вставка данных
-            for (int i = 1; i < main.getN() + 1; i++) {
-                if (i < 1000000)
-                    statement.executeUpdate("INSERT INTO TEST (FIELD) VALUES (" + i + ")");
+            if (main.getN() <= 1000000) {
+                for (int y = 1; y < main.getN() + 1; y++) {
+                    statement.executeUpdate("INSERT INTO TEST (FIELD) VALUES (" + y + ")");
+                }
+                //Получение и сохранение в List
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM test");
+                while (resultSet.next()) {
+                    list.add(resultSet.getInt(1));
+                }
+                //Работа с xml
+                try {
+                    DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+                    DocumentBuilder builder = builderFactory.newDocumentBuilder();
+
+                    Document document = builder.newDocument();
+                    Element rootElement = document.createElement("entries");
+                    document.appendChild(rootElement);
+
+                    for (int i = 0; i < list.size(); i++) {
+                        Element entryElement = document.createElement("entry");
+                        rootElement.appendChild(entryElement);
+
+                        Element fieldElement = document.createElement("field");
+                        fieldElement.appendChild(document.createTextNode(String.valueOf(list.get(i))));
+                        entryElement.appendChild(fieldElement);
+                    }
+
+                    TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                    Transformer transformer = transformerFactory.newTransformer();
+
+                    DOMSource source = new DOMSource(document);
+                    StreamResult result1 = new StreamResult(new File("1.xml"));
+                    transformer.transform(source, result1);
+
+                    main.transform("1.xml", "src/styleXSLT.xsl", "2.xml");
+                    //parsing 2.xml
+                    ArrayList<Integer> list1 = (ArrayList<Integer>) main.parsingXML("2.xml");
+                    int result = 0;
+                    for (int i = 0; i < list1.size(); i++) {
+                        result += list1.get(i);
+                    }
+                    //Result print
+                    System.out.println(result);
+
+                } catch (ParserConfigurationException e) {
+                    e.printStackTrace();
+                } catch (TransformerConfigurationException e) {
+                    e.printStackTrace();
+                } catch (TransformerException e) {
+                    e.printStackTrace();
+                }
             }
+
         } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/TEST", "pgadmin", "test");
-             Statement statement = connection.createStatement()) {
-
-            //Получение и сохранение в List
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM test");
-            while (resultSet.next()) {
-                list.add(resultSet.getInt(1));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        //Сохраняем в xml
-        try {
-            DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = builderFactory.newDocumentBuilder();
-
-            Document document = builder.newDocument();
-            Element rootElement = document.createElement("entries");
-            document.appendChild(rootElement);
-
-            for (int i = 0; i < list.size(); i++) {
-                Element entryElement = document.createElement("entry");
-                rootElement.appendChild(entryElement);
-
-                Element fieldElement = document.createElement("field");
-                fieldElement.appendChild(document.createTextNode(String.valueOf(list.get(i))));
-                entryElement.appendChild(fieldElement);
-            }
-
-            TransformerFactory transformerFactory = TransformerFactory.newInstance();
-            Transformer transformer = transformerFactory.newTransformer();
-
-            DOMSource source = new DOMSource(document);
-            StreamResult result1 = new StreamResult(new File("1.xml"));
-            transformer.transform(source, result1);
-
-            main.transform("1.xml", "src/styleXSLT.xsl", "2.xml");
-            ArrayList<Integer> list1 = (ArrayList<Integer>) main.parsingXML("2.xml");
-            int result = 0;
-            for (int i = 0; i < list1.size(); i++) {
-                result += list1.get(i);
-            }
-            System.out.println(result);
-
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        } catch (TransformerConfigurationException e) {
-            e.printStackTrace();
-        } catch (TransformerException e) {
             e.printStackTrace();
         }
 
